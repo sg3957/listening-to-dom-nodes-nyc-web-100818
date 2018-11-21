290c0e8079e1ef74bef0bf7919cfadcc4e966059
sg3957 <sg3957@nyu.edu>
2018-11-21 09:21:01 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,25 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+const main = document.getElementById('main');
+
+main.addEventListener('click', function(event) {
+  alert('I was clicked!');
+});
+
+const input = document.querySelector('input');
+
+input.addEventListener('keydown', function(e) {
+  console.log(e.which);
+});
+
+
+//prevent g from being pressed
+const input = document.querySelector('input')
+
+input.addEventListener('keydown', function(e) {
+  if (e.which === 71) {
+    return e.preventDefault()
+  } else {
+    console.log(e.which)
+  }
+});
